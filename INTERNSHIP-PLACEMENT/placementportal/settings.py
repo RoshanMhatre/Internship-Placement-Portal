a2190e78db962e2596f1b5d886e15ee0c1105318
Yash Mistry <76916876+yashms25@users.noreply.github.com>
2021-09-08 22:03:32 +0530

    Update settings.py

--- INTERNSHIP-PLACEMENT/placementportal/settings.py
+++ INTERNSHIP-PLACEMENT/placementportal/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yashmistry614@gmail.com'
-EMAIL_HOST_PASSWORD = 'pxcpeccmvruzjhim'
+EMAIL_HOST_USER = 'contactvitplacement@gmail.com'
+EMAIL_HOST_PASSWORD = 'dplygapftkxsqxib'
